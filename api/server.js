bd48a4d46411cdbedf0110570b5f70c4e6390c49
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-10 16:48:12 +0100

    imported app & mongoose, created port and serverURI

---
+++ api/server.js
@@ -0,0 +1,10 @@
+// import app
+const app = require('./app');
+// import mongoose from node_modules
+const mongoose = require('mongoose');
+
+const port = 8000;
+
+const serverURI = 'http://localhost:' + port;
+
+app.listen(port, () => console.log('Webserver: ' + serverURI));
