ff703acfa75a71a63386ae0e76b7a221888b65c4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 14:52:55 +0100

    added getProduct

--- api/models/products/productModel.js
+++ api/models/products/productModel.js
@@ -1,2 +1,13 @@
 const mongodb = require('mongoose');
-const Product = require('')
+const Product = require('./productSchema');
+
+exports.getProducts = (req,res)=>{
+    Product.find()
+    .then(data=> res.status(200).json(data))
+    .catch(err => res.status(500).json({
+        statusCode: 500,
+        status: false,
+        message: 'Failed to get products',
+        error: err
+    }))
+}
