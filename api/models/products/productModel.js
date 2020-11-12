fb8df89b148b7928f82112692215f88386d8f87b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 14:56:05 +0100

    added create product function

--- api/models/products/productModel.js
+++ api/models/products/productModel.js
@@ -22,3 +22,39 @@ exports.getOneProduct = (req, res) => {
         error: err
     }))
 }
+
+exports.createProduct = (req, res) => {
+    Product.find({ name: req.body.name })
+    .then(exists => {
+        if(exists.length > 0) {
+            return res.status(400).json({
+                statusCode: 400,
+                status: false,
+                message: 'A product by that name already exists, please update product instead'
+            })
+        }
+
+        const product = new Product({
+            name: req.body.name,
+            short: req.body.short,
+            desc: req.body.desc,
+            price: req.body.price,
+            image: req.body.image,
+        })
+        product.save()
+        .then(() => {
+            res.status(201).json({
+                statusCode: 201,
+                status: true,
+                message: 'Product created'
+            })
+        })
+        .catch(() => {
+            res.status(500).json({
+                statusCode: 500,
+                status: false,
+                message: 'Failed to create product'
+            })
+        })
+    })
+}
