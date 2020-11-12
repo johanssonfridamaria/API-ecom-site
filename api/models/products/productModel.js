b66397d3e8cd08e39d1cce9b8dbec56a01b98aef
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 14:54:27 +0100

    added get product

--- api/models/products/productModel.js
+++ api/models/products/productModel.js
@@ -11,3 +11,14 @@ exports.getProducts = (req,res)=>{
         error: err
     }))
 }
+
+exports.getOneProduct = (req, res) => {
+    Product.findById(req.params.id)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({
+        statusCode: 500,
+        status: false,
+        message: 'Failed to get product',
+        error: err
+    }))
+}
