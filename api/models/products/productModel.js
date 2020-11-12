f9958568049a7602abb847a5dba8055cb91a2f52
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 14:59:32 +0100

    added update and delete product function

--- api/models/products/productModel.js
+++ api/models/products/productModel.js
@@ -58,3 +58,42 @@ exports.createProduct = (req, res) => {
         })
     })
 }
+
+exports.updateProduct = (req, res) => {
+    Product.updateOne({ _id: req.params.id }, req.body)
+    .then(() => {
+        Product.updateOne({ _id: req.params.id }, { $set: { modified: Date.now()}})
+        .then(() => {
+            res.status(200).json({
+                statusCode: 200,
+                status: true,
+                message: 'Product updated'
+            })
+        })
+    })
+    .catch(() => {
+        res.status(500).json({
+            statusCode: 500,
+            status: false,
+            message: 'Failed to update product'
+        })
+    })
+}
+
+exports.deleteProduct = (req, res) => {
+    Product.deleteOne({ _id: req.params.id })
+    .then(() => {
+        res.status(200).json({
+            statusCode: 200,
+            status: true,
+            message: 'Product deleted!'
+        })
+    })
+    .catch(() => {
+        res.status(500).json({
+            statusCode: 500,
+            status: false,
+            message: 'Failed to delete product'
+        })
+    })
+}
