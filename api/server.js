5414901563c7884c4073d9d8521fb99051a94327
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 10:23:16 +0100

    connected to DB

--- api/server.js
+++ api/server.js
@@ -3,9 +3,18 @@ const app = require('./app');
 // import mongoose from node_modules
 const mongoose = require('mongoose');
 
-const port = 8000;
+//create port
+const port = process.env.port || 8000;
+require('dotenv').config();
 
 const serverURI = 'http://localhost:' + port;
 const mongoURI = process.env.MONGO_URI;
 
-app.listen(port, () => console.log('Webserver: ' + serverURI));
+app.listen(port, () => console.log('Webserver running at: ' + serverURI));
+
+mongoose
+.set('useCreateIndex', true)
+.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, ()=> console.log('Connected to DB'));
