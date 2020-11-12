9a970fbe164c45481f5b05a19b9ff001deb4e0a5
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 14:55:22 +0100

    added get one product

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -3,5 +3,8 @@ const productModel = require('../models/products/productModel');
 const auth = require('../authentication/auth');
 
 router.get('/', productModel.getProducts);
+router.get('/:id', productModel.getOneProduct);
+
+
 
 module.exports = router;
