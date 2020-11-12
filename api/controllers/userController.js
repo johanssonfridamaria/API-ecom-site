c7c03e13bc010d25b368903066d6a1b413d512c4
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 11:35:40 +0100

    added get end point

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,5 +3,6 @@ const userModel = require('../models/users/userModel');
 
 router.post('/register', userModel.registerUser);
 router.post('/login', userModel.loginUser);
+router.get('/', userModel.getUsers);
 
 module.exports = router;
