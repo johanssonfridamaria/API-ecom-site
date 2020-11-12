46564d37edf0cbd9119bcf182a969a9a6a6f1ca6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-12 10:09:16 +0100

    added db name

--- api/server.js
+++ api/server.js
@@ -6,6 +6,6 @@ const mongoose = require('mongoose');
 const port = 8000;
 
 const serverURI = 'http://localhost:' + port;
-const mongoURI = 'mongodb+srv://frida:bytmig123@cluster0.2xfoe.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://frida:bytmig123@cluster0.2xfoe.mongodb.net/ECOM?retryWrites=true&w=majority';
 
 app.listen(port, () => console.log('Webserver: ' + serverURI));
