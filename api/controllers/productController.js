ee85c991eb6430930662b207f3442421ad72da58
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-13 11:12:44 +0100

    added verifytoken

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -5,11 +5,11 @@ const auth = require('../authentication/auth');
 router.get('/', productModel.getProducts);
 router.get('/:id', productModel.getOneProduct);
 
-router.post('/new',productModel.createProduct);
+router.post('/new',auth.verifyToken, productModel.createProduct);
 
-router.patch('/:id', productModel.updateProduct);
+router.patch('/:id',auth.verifyToken, productModel.updateProduct);
 
-router.delete('/:id', productModel.deleteProduct);
+router.delete('/:id',auth.verifyToken, productModel.deleteProduct);
 
 
 module.exports = router;
