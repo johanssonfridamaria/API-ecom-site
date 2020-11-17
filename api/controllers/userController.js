990f26384dd50a3b27dd2315608e3bb3145c332b
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-17 14:32:41 +0100

    added swagger definitions

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userModel = require('../models/users/userModel');
 
+
 /**
  * @swagger
  * definitions:
@@ -8,7 +9,7 @@ const userModel = require('../models/users/userModel');
  *     properties:
  *       firstName:
  *         type: string
- *      lastName:
+ *       lastName:
  *         type: string
  *       email:
  *         type: string
@@ -16,8 +17,52 @@ const userModel = require('../models/users/userModel');
  *         type: string
  */
 
+  /**
+ * @swagger
+ * /api/users/register:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: Register a new user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: User
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: Successfully registered
+ */
 
 router.post('/register', userModel.registerUser);
+
+  /**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: Login a user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: User
+ *         description: User email & Password
+ *         in: body
+ *         required: true
+ *         properties:
+ *            email:
+ *              type: string
+ *            password:
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: Successfully signed in
+ */
 
 router.post('/login', userModel.loginUser);
 
@@ -34,12 +79,58 @@ router.post('/login', userModel.loginUser);
  *          200:
  *              description: An array of Users
  *              schema:
- *                  $ref: '#/definitions/Users'
+ *                  $ref: '#/definitions/User'
  */
 
 router.get('/', userModel.getUsers);
 
+/**
+ * @swagger
+ * /api/users/{email}:
+ *   patch:
+ *     tags:
+ *       - Users
+ *     description: Updates a single user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User email
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: User
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+
 router.patch('/:email', userModel.updateUser);
+
+/**
+ * @swagger
+ * /api/users/{email}:
+ *   delete:
+ *     tags:
+ *       - Users
+ *     description: Deletes a single user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User email
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 
 router.delete('/:email', userModel.deleteUser);
 
