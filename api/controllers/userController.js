a14b0d2d77f8e102e8f11a5dd6d977d51ca57b9f
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-17 14:04:05 +0100

    added swagger definition to get users

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,20 +1,43 @@
 const router = require('express').Router();
 const userModel = require('../models/users/userModel');
 
+/**
+ * @swagger
+ * definitions:
+ *   User:
+ *     properties:
+ *       firstName:
+ *         type: string
+ *      lastName:
+ *         type: string
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+
 router.post('/register', userModel.registerUser);
 
 router.post('/login', userModel.loginUser);
 
-router.get('/', userModel.getUsers);
 /**
  * @swagger
- * /users:
+ * /api/users:
  *      get:
- *          description: Get all users
- *          responses:
- *              200:
- *                    description: Success
+ *      tags:
+ *       - Users
+ *      description: Get all users
+ *      produces:
+ *       - application/json
+ *      responses:
+ *          200:
+ *              description: An array of Users
+ *              schema:
+ *                  $ref: '#/definitions/Users'
  */
+
+router.get('/', userModel.getUsers);
 
 router.patch('/:email', userModel.updateUser);
 
