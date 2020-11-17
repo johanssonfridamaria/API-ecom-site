0ec0af82e690f05ff0d29d3ad2f9a21a60c78cf6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-17 13:52:31 +0100

    added swagger definitions

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,28 +1,142 @@
 const router = require('express').Router();
 const productModel = require('../models/products/productModel');
 const auth = require('../authentication/auth');
-const app= require('../app');
+const app = require('../app');
+
+/**
+ * @swagger
+ * definitions:
+ *   Product:
+ *     properties:
+ *       name:
+ *         type: string
+ *       brand:
+ *         type: string
+ *       short:
+ *         type: string
+ *       desc:
+ *         type: string
+ *       price:
+ *         type: integer
+ *       image:
+ *         type: string
+ */
 
 
-router.get('/', productModel.getProducts);
 /**
  * @swagger
  * /products:
  *      get:
- *          description: Get all products
- *          responses:
- *              200:
- *                    description: Success
+ *      tags:
+ *       - Products
+ *      description: Get all products
+ *      produces:
+ *       - application/json
+ *      responses:
+ *          200:
+ *              description: An array of Products
+ *              schema:
+ *                  $ref: '#/definitions/Product'
  */
 
- 
+router.get('/', productModel.getProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     tags:
+ *       - Products
+ *     description: Returns a single product
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Product id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: A single product
+ *         schema:
+ *           $ref: '#/definitions/Product'
+ */
+
 router.get('/:id', productModel.getOneProduct);
 
-router.post('/new',auth.verifyToken, productModel.createProduct);
+ /**
+ * @swagger
+ * /products/new:
+ *   post:
+ *     tags:
+ *       - Products
+ *     description: Creates a new product
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: Product
+ *         description: Product object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Product'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 
-router.patch('/:id',auth.verifyToken, productModel.updateProduct);
+router.post('/new', auth.verifyToken, productModel.createProduct);
 
-router.delete('/:id',auth.verifyToken, productModel.deleteProduct);
+/**
+ * @swagger
+ * /products/{id}:
+ *   patch:
+ *     tags:
+ *       - Products
+ *     description: Updates a single product
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Product id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: Product
+ *         description: Product object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Product'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
+
+router.patch('/:id', auth.verifyToken, productModel.updateProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     tags:
+ *       - Products
+ *     description: Deletes a single product
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Product id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
+
+router.delete('/:id', auth.verifyToken, productModel.deleteProduct);
 
 
 module.exports = router;
